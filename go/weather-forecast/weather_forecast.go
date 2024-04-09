720cd9af20bc9543c363c4ba4196b7f2acc0e481
Dan Beste <drb@wishalloy.com>
2024-04-09 15:30:14 -0500

    Tuesday exercises

---
+++ go/weather-forecast/weather_forecast.go
@@ -0,0 +1,13 @@
+// Package weather attempts to forecast the weather in various cities across Goblinocus.
+package weather
+
+// CurrentCondition current weather condition.
+var CurrentCondition string
+// CurrentLocation current location in Goblinocus.
+var CurrentLocation string
+
+// Forecast returns a formatted forcast for a location passed the city and condition.
+func Forecast(city, condition string) string {
+	CurrentLocation, CurrentCondition = city, condition
+	return CurrentLocation + " - current weather condition: " + CurrentCondition
+}
