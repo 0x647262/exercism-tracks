40d93d479264a09bc1fc0a3da0e2506437af62e8
Dan Beste <drb@wishalloy.com>
2024-04-18 03:04:21 -0500

    Clojure interest-is-interesting

---
+++ clojure/interest-is-interesting/src/interest_is_interesting.clj
@@ -0,0 +1,22 @@
+(ns interest-is-interesting)
+
+(defn interest-rate
+  "Returns the interest rate based on the specified balance."
+  [balance]
+  (cond
+    (>= balance 5000M) 2.475
+    (>= balance 1000M) 1.621
+    (>= balance 0M) 0.5
+    :else -3.213))
+
+(defn annual-balance-update
+  "Returns the annual balance update, taking into account the interest rate."
+  [balance]
+  (let [delta (* (bigdec (interest-rate balance)) 0.01M (abs balance))]
+    (+ balance delta)))
+
+(defn amount-to-donate
+  "Returns how much money to donate based on the balance and the tax-free percentage."
+  [balance tax-free-percentage]
+  (let [unexplained-doubling (bigdec (* tax-free-percentage 2))]
+    (max 0 (int (* balance (/ unexplained-doubling 100))))))
